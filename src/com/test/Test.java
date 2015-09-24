fc6b38f65b290d3ec204ef3a33bc2bca113657d9
王青 <wangqing@gigold.com>
2015-09-24 15:11:04 +0800

    test

---
+++ src/com/test/Test.java
@@ -0,0 +1,20 @@
+/**
+ * Title: Test.java<br/>
+ * Description: <br/>
+ * Copyright: Copyright (c) 2015<br/>
+ * Company: gigold<br/>
+ *
+ */
+package com.test;
+
+/**
+ * Title: Test<br/>
+ * Description: <br/>
+ * Company: gigold<br/>
+ * @author wangqing
+ * @date 2015年9月24日下午3:03:59
+ *
+ */
+public class Test {
+
+}
